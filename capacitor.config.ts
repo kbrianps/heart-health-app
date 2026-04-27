0a9f3e4e174df5f39a9a9cedadacde8c465d91d5
Brian Pravato <kbrianps@gmail.com>
2026-04-27 13:48:49 -0300

    chore: bootstrap ionic react + vite + capacitor com typescript

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kbrianps.heart_health_app',
+  appName: 'Saúde Cardíaca',
+  webDir: 'dist',
+};
+
+export default config;
